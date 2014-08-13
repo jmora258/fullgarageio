7f3b8fbe0ea163e9ffe8e1cc164c0332cfd5217e
Justino Mora <jmora258@gmail.com>
2014-08-13 16:24:16 -0700

    create users migration and run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813004523) do
+ActiveRecord::Schema.define(version: 20140813033613) do
 
   create_table "cars", force: true do |t|
     t.string   "make"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20140813004523) do
     t.string   "name"
     t.integer  "mileage"
     t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
