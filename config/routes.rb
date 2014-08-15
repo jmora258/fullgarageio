6963018624f1d5b7dccab882c6f854a8335e988e
Justino Mora <jmora258@gmail.com>
2014-08-14 21:55:03 -0700

    update pages controller with home action to redirect people to home page, update the home page with simple welcome message, and update routes file with resources :cars

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
 
   get '/' => 'pages#home'
 
+  resources :car 
+
+
+  # get '/cars/:id' => 'cars#show'
+  # get '/cars' => 'cars#new'
+  # post '/cars' => 'cars#create'
+  # get '/cars/:id/edit' => 'cars#edit'
+  # put '/cars/:id' => '/cars#update'
+  # delete '/cars/:id' => 'cars#destroy'
+
 end
